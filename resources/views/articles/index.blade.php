c0af82cfa00486ac5aa38fc1c5977a5e1b06106a
Darren Henderson <darrenh@Darrens-MacBook-Pro.local>
2019-12-24 19:05:38 -0600

    Finish video 32.

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,7 +3,7 @@
 @section ('content')
   <div id="wrapper">
     <div id="page" class="container">
-      @foreach ($articles as $article)
+      @forelse ($articles as $article)
           <div id="content">
               <div class="title">
                   <h2>
@@ -22,7 +22,9 @@
 
                 {{ $article->excerpt }}
             </div>
-      @endforeach
+      @empty
+        <p>No relevant articles yet.</p>
+      @endforelse
     </div>
   </div>
 @endsection
